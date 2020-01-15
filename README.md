2d4e623b541a86665ce612eeca51c26105a7b45f
Brian Wing <vision0709@pm.me>
2020-01-14 20:17:18 -0500

    Initial backup from GitHub

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Keychain
+# keychain
+
+A secure password generation and storage tool for Android and iOS created using Flutter and Dart.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](http://flutter.io/).
---
+++ android/app/src/main/java/com/wing/brian/keychain/keychain/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wing.brian.keychain.keychain;
+
+import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
